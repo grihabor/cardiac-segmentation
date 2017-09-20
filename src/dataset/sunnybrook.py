c6fc09726bc29f67e8d9ca4316dd02f8f99194a6
Borodin Gregory <grihabor@mail.ru>
2017-09-20 22:01:48 +0300

    process xlsx mapping

--- src/dataset/sunnybrook.py
+++ src/dataset/sunnybrook.py
@@ -3,14 +3,16 @@ import itertools
 import logging
 import tempfile
 import zipfile
-
 import shutil
+
+from openpyxl import load_workbook
 
 from dataset.dicom_reader import read_dicom_image
 from utils import save_as_png
 
 DIR_CACHE = '/data/cache'
 
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -30,6 +32,7 @@ def clean_cache():
             path = os.path.join(root_path, filename)
             try:
                 os.remove(path)
+                logger.debug(f'Remove {path}')
             except FileNotFoundError as e:
                 logger.warning(e)
 
@@ -38,8 +41,7 @@ def read_sunnybrook_images(path):
     clean_cache()
 
     with zipfile.ZipFile(path, 'r') as zip_file:
-        for zip_path in filter_dicom(zip_file.namelist()):
-            print(zip_path)
+        for zip_path in filter_dicom(zip_file.namelist(), limit=5):
 
             with tempfile.NamedTemporaryFile('wb') as temp_dicom:
                 with zip_file.open(zip_path, 'r') as zipped_dicom:
@@ -50,3 +52,16 @@ def read_sunnybrook_images(path):
             name = filename.split('.')[0]
             img_path = os.path.join(DIR_CACHE, f'{name}.png')
             save_as_png(img, img_path)
+
+
+def read_sunnybrook_mapping(path):
+    wb = load_workbook(path)
+    ws = wb.worksheets[0]
+    header = ws.rows[0]
+    for row in ws.iter_rows():
+        print(row)
+
+
+def create_sunnybrook_tfrecords():
+    read_sunnybrook_mapping()
+
